c8bab8a562d5b812c6aae70273e1c3fcecaec671
NotMaxwell <william.max.mccormick@gmail.com>
2026-04-17 17:31:12 -0500

    finished through 36

--- exercises/025_errors5.zig
+++ exercises/025_errors5.zig
@@ -26,11 +26,10 @@ fn addFive(n: u32) MyNumberError!u32 {
     // This function needs to return any error which might come back from detect().
     // Please use a "try" statement rather than a "catch".
     //
-    const x = detect(n);
+    const x = try detect(n);
 
     return x + 5;
 }
-
 fn detect(n: u32) MyNumberError!u32 {
     if (n < 10) return MyNumberError.TooSmall;
     if (n > 20) return MyNumberError.TooBig;
